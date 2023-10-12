1e68c924f157e44a0df6e112489c23275c432800
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2023-10-12 08:30:28 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hELLOW wORLD");
+		
+		try {
+            // Specify the URL of your Laravel API endpoint
+            String apiUrl = "https://api.publicapis.org/entries"; // Update with your actual API endpoint
+
+            // Create a URL object
+            URL url = new URL(apiUrl);
+
+            // Open a connection to the URL
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            // Set the request method to GET
+            connection.setRequestMethod("GET");
+
+            // Get the response code
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response Code: " + responseCode);
+
+            // Read the response content
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            StringBuilder response = new StringBuilder();
+
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+
+            // Print the response content
+            System.out.println("Response Content: " + response.toString());
+
+            // Close the connection
+            connection.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+	}
+
+}
